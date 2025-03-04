878b7c90d0ebbfa9d49ce2b250096e3eecc9d0f2
CardenasTiago <cardenas.tiago.r@gmail.com>
2025-03-04 19:57:12 +0000

    Cambio en los planes

--- src/models/PlanD.java
+++ src/models/PlanD.java
@@ -1,19 +1,36 @@
 package models;
 
+import java.util.List;
+
 public class PlanD extends PlanEstudio {
-    public PlanD(String descripcion) {
-        super(descripcion); // Descripción personalizada
+ public PlanD(String descripcion) {
+        super(descripcion);
     }
 
     @Override
     public boolean puedeRecursarMateria(Estudiante estudiante, Materia materia) {
-        boolean correlativasAprobadas = materia.getCorrelativas().stream()
-                .allMatch(correlativa -> estudiante.getMateriasAprobadas().contains(correlativa));
-
-        boolean finalesAprobados = estudiante.getCarrera().getTodasLasMaterias().stream()
-                .filter(m -> m.getCuatrimestre() <= materia.getCuatrimestre() - 3)
-                .allMatch(m -> estudiante.getMateriasAprobadas().contains(m));
-
-        return correlativasAprobadas && finalesAprobados;
+        // Verificar correlativas aprobadas
+        List<Materia> correlativas = materia.getCorrelativas();
+        List<Materia> materiasAprobadas = estudiante.getMateriasAprobadas();
+        
+        for (Materia correlativa : correlativas) {
+            if (!materiasAprobadas.contains(correlativa)) {
+                return false;
+            }
+        }
+        
+        // Verificar finales aprobados de 3 cuatrimestres previos
+        List<Materia> todasLasMaterias = estudiante.getCarrera().getTodasLasMaterias();
+        int cuatrimestreMateria = materia.getCuatrimestre();
+        
+        for (Materia m : todasLasMaterias) {
+            if (m.getCuatrimestre() <= cuatrimestreMateria - 3) {
+                if (!materiasAprobadas.contains(m)) {
+                    return false;
+                }
+            }
+        }
+        
+        return true;
     }
 }
